05b18632cba81f85c3592f0942036aef356e514f
Katze033 <josh.katzenmeyer@gmail.com>
2019-11-30 13:49:39 -0600

    Updated blog styles and installed nuka carousel.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -7,6 +7,13 @@ import blogStyles from '../components/modules/blog.module.css'
 
 export const query = graphql`
   query($slug: String!) {
+    allWordpressCategory {
+      edges {
+        node {
+          name
+        }
+      }
+    }
     allWordpressPost(filter: { slug: { eq: $slug } }) {
       edges {
         node {
@@ -29,11 +36,59 @@ export default ({ data }) => {
   return (
     <Layout>
       <div className={blogStyles.blog_container}>
-        <h1 className={blogStyles.blog_title} dangerouslySetInnerHTML={{ __html: post.title }} />
-        <p className={blogStyles.blog_date}>{post.date}</p>
-        <p><Link to="/blog"  className={blogStyles.blog_link}>Go Back</Link></p>
-        <p className={blogStyles.blog_content} dangerouslySetInnerHTML={{ __html: post.content }} />
+        <div className={blogStyles.blogContent_container}>
+          <div className={blogStyles.blogPost_container}>
+            <h1 className={blogStyles.blog_title} dangerouslySetInnerHTML={{ __html: post.title }} />
+            <p className={blogStyles.blog_date}>{post.date}</p>
+            <p><Link to="/blog" className={blogStyles.blog_link}>Go Back</Link></p>
+            <p className={blogStyles.blog_content} dangerouslySetInnerHTML={{ __html: post.content }} />
+          </div>
+        </div>
+        <div className={blogStyles.blogNav_container}>
+          <h2>Recent Posts</h2>
+          <ol>
+            {data.allWordpressPost.edges.map((edge, i) => {
+              if (i < 5) {
+                return (
+                  <div className={blogStyles.blogNavList_container}>
+                    <li className={blogStyles.blogNav_list}>
+                      <h3><Link to={`/blog/${edge.node.slug}`} className={blogStyles.blogNav_link} dangerouslySetInnerHTML={{ __html: edge.node.title }}></Link></h3>
+                    </li>
+                  </div>
+                )
+              }
+            })}
+          </ol>
+          <h2>Archive</h2>
+          <ol>
+            {data.allWordpressPost.edges.map((edge, i) => {
+              if (i < 12) {
+                return (
+                  <div className={blogStyles.blogNavList_container}>
+                    <li className={blogStyles.blogNav_list}>
+                      <h3><Link to={`/blog/${edge.node.slug}`} className={blogStyles.blogNav_link} dangerouslySetInnerHTML={{ __html: edge.node.date }}></Link></h3>
+                    </li>
+                  </div>
+                )
+              }
+            })}
+          </ol>
+          <h2>Categories</h2>
+          <ol>
+            {data.allWordpressCategory.edges.map((edge, i) => {
+              if (i < 5) {
+                return (
+                  <div className={blogStyles.blogNavList_container}>
+                    <li className={blogStyles.blogNav_list}>
+                      <h3><Link to={`/blog/${edge.node.slug}`} className={blogStyles.blogNav_link} dangerouslySetInnerHTML={{ __html: edge.node.name }}></Link></h3>
+                    </li>
+                  </div>
+                )
+              }
+            })}
+          </ol>
+        </div>
       </div>
-    </Layout>
+    </Layout >
   )
 }
