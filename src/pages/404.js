0c133e2d09dbbf4caff60725d94adf43f73ad7e7
Katze033 <josh.katzenmeyer@gmail.com>
2019-12-29 19:41:13 -0600

    Added custom 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from '../components/layout'
+
+const RedirectPage = () => {
+    return (
+        <Layout>
+            <div>
+                <p>Sorry, there is nothing here.</p>
+            </div>
+        </Layout>
+    )
+}
+
+export default RedirectPage
