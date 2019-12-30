d4015ee0711a210c80f9889b8e7ea3ca130e3821
Katze033 <josh.katzenmeyer@gmail.com>
2019-12-29 18:20:51 -0600

    Finished mobile responsiveness. Next: tweak comments section on posts.

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,9 +8,9 @@ const Footer = () => {
             <div className={footerStyles.footer_container}>
                 <div className={footerStyles.footer_social}>
                     <h3>Stay Connected</h3>
-                    <i class="fab fa-instagram"></i>
-                    <i class="fab fa-facebook"></i>
-                    <i class="fab fa-pinterest"></i>
+                    <a href="https://www.instagram.com/questforcute/" target="__blank"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.facebook.com/questforcute/" target="__blank"><i class="fab fa-facebook"></i></a>
+                    <a href="https://www.pinterest.com/questforcute/" target="__blank"><i class="fab fa-pinterest"></i></a>
                 </div>
                 <div className={footerStyles.footer_copyright}>
                     <p>&copy; 2020 Quest For Cute </p>
