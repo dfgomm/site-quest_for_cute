60d51ba9893011333d666361eb08edaa07548506
Katze033 <josh.katzenmeyer@gmail.com>
2020-01-26 15:21:34 -0600

    Revert git.

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,6 +71,7 @@ const IndexPage = () => {
                             return (
                                 <div className={indexStyles.index_post}>
                                     <h2><Link to={`/blog/${edge.node.slug}`} className={indexStyles.post_title} dangerouslySetInnerHTML={{ __html: edge.node.title }}></Link></h2>
+                                    <p className={indexStyles.post_date} dangerouslySetInnerHTML={{ __html: edge.node.date}} />
                                     <p className={indexStyles.post_excerpt} dangerouslySetInnerHTML={{ __html: edge.node.excerpt }} />
                                     <p><Link to={`/blog/${edge.node.slug}`} className={indexStyles.post_link}>Read more</Link></p>
                                 </div>
@@ -84,5 +85,3 @@ const IndexPage = () => {
 }
 
 export default IndexPage
-
-//                                    <p className={indexStyles.post_excerpt} dangerouslySetInnerHTML={{ __html: edge.node.excerpt }} />
