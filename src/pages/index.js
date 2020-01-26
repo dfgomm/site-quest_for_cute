184de88382202c96a9b54744829849d395997a32
Katze033 <josh.katzenmeyer@gmail.com>
2020-01-25 23:39:27 -0600

    Removing css rules didn't do anything. Now flipping date and excerpt.

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,8 +71,8 @@ const IndexPage = () => {
                             return (
                                 <div className={indexStyles.index_post}>
                                     <h2><Link to={`/blog/${edge.node.slug}`} className={indexStyles.post_title} dangerouslySetInnerHTML={{ __html: edge.node.title }}></Link></h2>
-                                    <p className={indexStyles.post_date} dangerouslySetInnerHTML={{ __html: edge.node.date}} />
                                     <p className={indexStyles.post_excerpt} dangerouslySetInnerHTML={{ __html: edge.node.excerpt }} />
+                                    <p className={indexStyles.post_date} dangerouslySetInnerHTML={{ __html: edge.node.date}} />
                                     <p><Link to={`/blog/${edge.node.slug}`} className={indexStyles.post_link}>Read more</Link></p>
                                 </div>
                             )
@@ -85,3 +85,5 @@ const IndexPage = () => {
 }
 
 export default IndexPage
+
+//                                    <p className={indexStyles.post_excerpt} dangerouslySetInnerHTML={{ __html: edge.node.excerpt }} />
