f76286587994f250847b4210f6333db031e6aae2
Katze033 <josh.katzenmeyer@gmail.com>
2020-01-26 15:58:55 -0600

    Nope. Added date before excerpt to see if it works without dangerously setting inner html...not likely.

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,8 +71,8 @@ const IndexPage = () => {
                             return (
                                 <div className={indexStyles.index_post}>
                                     <h2><Link to={`/blog/${edge.node.slug}`} className={indexStyles.post_title} dangerouslySetInnerHTML={{ __html: edge.node.title }}></Link></h2>
-                                    <p className={indexStyles.post_excerpt} dangerouslySetInnerHTML={{ __html: edge.node.excerpt }} />
                                     <p>{edge.node.date}</p>
+                                    <p className={indexStyles.post_excerpt} dangerouslySetInnerHTML={{ __html: edge.node.excerpt }} />
                                     <p><Link to={`/blog/${edge.node.slug}`} className={indexStyles.post_link}>Read more</Link></p>
                                 </div>
                             )
